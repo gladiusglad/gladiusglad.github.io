ad79eed5c1bcfcdb962162e0115015443e23d703
GladGladius <defoverthought@gmail.com>
2021-02-04 16:36:48 +0700

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
