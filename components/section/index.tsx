73c0cd55c7d51f8bdfd339358c4465cd0e867b89
GladGladius <defoverthought@gmail.com>
2021-04-06 11:23:50 +0700

    Align black section back title to right

--- components/section/index.tsx
+++ components/section/index.tsx
@@ -10,30 +10,31 @@ import styles from './style.module.scss'
 const backTitleStartLength = 2,
   backTitleEndRepeat = 5;
 
-const backTitleKeyframes = keyframes`
-  from {
-    transform: translate3d(-100em, 0, 0);
-    letter-spacing: 5em;
-  }
-  to {
-    transform: translate3d(0, 0, 0);
-    letter-spacing: normal;
-  }
-`
-
 interface SectionProps {
   title: string,
   black?: boolean
 }
 
 export default function Section({title, black, children}: React.PropsWithChildren<SectionProps>) {
-  let backTitle = title.toLowerCase().replace(/\s/g, ''),
-    backTitleStart = backTitle.substring(backTitle.length - backTitleStartLength),
-    backTitleEnd = backTitle.repeat(backTitleEndRepeat);
+  const backTitle = title.toLowerCase().replace(/\s/g, ''),
+    backTitleStart = black ?
+      backTitle.substring(0, backTitleStartLength) :
+      backTitle.substring(backTitle.length - backTitleStartLength),
+    backTitleEnd = backTitle.repeat(backTitleEndRepeat),
+    backTitleKeyframes = keyframes`
+      from {
+        transform: translate3d(${black ? '' : '-'}100em, 0, 0);
+        letter-spacing: 5em;
+      }
+      to {
+        transform: translate3d(0, 0, 0);
+        letter-spacing: normal;
+      }
+    `
 
   return <div className={c(styles.section, [black, styles.black])}>
     <Fade keyframes={backTitleKeyframes} className={styles.backTitleFade} duration={2000} >
-      <h1 className={styles.backTitle}>{backTitleStart}<span className={styles.bold}>{backTitle}</span>{backTitleEnd}</h1>
+      <h1 className={styles.backTitle}>{black ? backTitleEnd : backTitleStart}<span className={styles.bold}>{backTitle}</span>{black ? backTitleStart : backTitleEnd}</h1>
     </Fade>
     <Container className={`content ${styles.container}`}>
       <h1 className={styles.title}>{title}</h1>
