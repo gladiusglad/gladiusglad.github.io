b18b1c4d196c4f55935334b8b5b76cb15fe0f3f1
GladGladius <defoverthought@gmail.com>
2021-04-07 22:57:33 +0700

    Extract BackTitle component

---
+++ components/backTitle/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Fade from '../fade'
+import { keyframes } from "@emotion/react"
+
+import c from '../../lib/className'
+
+import styles from './style.module.scss'
+
+interface BackTitleProps {
+  title: string,
+  right?: boolean,
+  startLength?: number,
+  endRepeat?: number,
+  noAnim?: boolean
+}
+
+export default function BackTitle({title, right, startLength = 2, endRepeat = 5, noAnim}: BackTitleProps) {
+  const backTitle = title.toLowerCase().replace(/\s/g, ''),
+    backTitleStart = right ?
+      backTitle.substring(0, startLength) :
+      backTitle.substring(backTitle.length - startLength),
+    backTitleEnd = backTitle.repeat(endRepeat),
+    backTitleKeyframes = keyframes`
+      from {
+        transform: translate3d(${right ? '' : '-'}${7.5 * title.length + 62.5}em, 0, 0);
+        letter-spacing: 5em;
+      }
+      to {
+        transform: translate3d(0, 0, 0);
+        letter-spacing: normal;
+      }
+    `
+
+  const h1 = (
+    <h1 className={c(styles.backTitle, [right, styles.right])}>
+      {right ? backTitleEnd : backTitleStart}
+      <span className={styles.bold}>{backTitle}</span>
+      {right ? backTitleStart : backTitleEnd}
+    </h1>
+  )
+
+  return <>
+    {noAnim ? h1 :
+      <Fade keyframes={backTitleKeyframes} className={styles.backTitleFade} duration={2000} >
+        {h1}
+      </Fade>
+    }
+  </>
+}
--- components/section/index.tsx
+++ components/section/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import Container from '../container'
-import Fade from '../fade'
-import { keyframes } from "@emotion/react"
+import BackTitle from '../backTitle'
 
 import c from '../../lib/className'
 
 import styles from './style.module.scss'
-
-const backTitleStartLength = 2,
-  backTitleEndRepeat = 5;
 
 interface SectionProps {
   title: string,
@@ -16,26 +12,8 @@ interface SectionProps {
 }
 
 export default function Section({title, black, children}: React.PropsWithChildren<SectionProps>) {
-  const backTitle = title.toLowerCase().replace(/\s/g, ''),
-    backTitleStart = black ?
-      backTitle.substring(0, backTitleStartLength) :
-      backTitle.substring(backTitle.length - backTitleStartLength),
-    backTitleEnd = backTitle.repeat(backTitleEndRepeat),
-    backTitleKeyframes = keyframes`
-      from {
-        transform: translate3d(${black ? '' : '-'}100em, 0, 0);
-        letter-spacing: 5em;
-      }
-      to {
-        transform: translate3d(0, 0, 0);
-        letter-spacing: normal;
-      }
-    `
-
   return <div className={c(styles.section, [black, styles.black])}>
-    <Fade keyframes={backTitleKeyframes} className={styles.backTitleFade} duration={2000} >
-      <h1 className={styles.backTitle}>{black ? backTitleEnd : backTitleStart}<span className={styles.bold}>{backTitle}</span>{black ? backTitleStart : backTitleEnd}</h1>
-    </Fade>
+    <BackTitle title={title} right={black} />
     <Container className={`content ${styles.container}`}>
       <h1 className={styles.title}>{title}</h1>
       {children}
