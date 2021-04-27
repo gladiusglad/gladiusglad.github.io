ccc52a5eac07c232a77de83128b3720d5fff35b4
gladiusglad <defoverthought@gmail.com>
2021-04-27 20:14:43 +0700

    Animate navbar icon

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 import styles from './style.module.scss'
 
 export default function Navbar() {
   return (
-    <nav className={styles.nav}>
-      <a href="/" className={styles.navLogo}>
+    <Link href="/">
+      <a className={styles.navLogo}>
         <Image unoptimized src="/img/navbar-logo.png" layout="intrinsic" width={72} height={72} alt="menu" />
       </a>
-    </nav>
+    </Link>
   )
 }
